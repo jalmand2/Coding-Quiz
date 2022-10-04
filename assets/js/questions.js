21213ae4c22cecff33bcc9ac4482076517e196a9
jalmand2 <jlalmand27@gmail.com>
2022-10-04 17:21:31 -0400

    Added comments, classes and tags to HTML. Also updated the scores javascript to the correct code. Added the questions to the questions javascript.

---
+++ assets/js/questions.js
@@ -0,0 +1,31 @@
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings,", "booleans", "alerts", "numbers"],
+        answer: "alerts",
+    }, 
+    {
+        title: "The condition in an if / else statement is enclosed within _____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses",
+    },
+    {
+        title: "Arrays in JavaScript can be used to store ____.",
+        choices: [
+        "numbers and strings", 
+        "other arrays",
+        "booleans",
+        "all of the above",
+    ],
+        answer: "all of the above",
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: [
+            "single quotes", 
+            "double quotes",
+            "both A and B",
+            "none of the above",
+        ],
+    }
+]
--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,104 +1,21 @@
-// variables to keep track of quiz state
-    // currentQuestion
-    // time
-    // timerId
-
-// variables to reference DOM elements
-var questionsEl = document.getElementById('questions');
-
-
-/// FUNCTION TO START THE QUIZ
-function startQuiz() {
-  // hide start screen 
+function printHighscores() {
+    // either get scores from localstorage or set to empty array
   
-  // un-hide questions section
-
-  // start timer
-
-  // show starting time
-
-  getQuestion();
-}
-
-/// FUNCTION TO GET/SHOW EACH QUESTION ///
-function getQuestion() {
-  // get current question object from array
-
-  // update title with current question
-
-  // clear out any old question choices
-
-  // loop over choices
-    // FOR {
-      // create new button for each choice
+    // sort highscores by score property in descending order
   
-      // display on the page
-      
-    // } 
-}
-
-/// FUNCTION FOR CLICKING A QUESTION ///
-function questionClick(event) {
-
-  // if the clicked element is not a choice button, do nothing.
-  if (something) {
-
+    // loop through scores
+      // create li tag for each high score
+  
+      // display on page
   }
-
-  if (something) {
-  // check if user guessed wrong
-    // penalize time
-
-    // display new time on page
-
-    // give them feedback, letting them know it's wrong
-  } else {
-    // give them feedback, letting them know it's right
+  
+  /// FUNCTION TO CLEAR SCORES ///
+  function clearHighscores() {
+    // remove an item from local storage
+    // reload the page
   }
-
-  // flash right/wrong feedback on page for a short period of time
-
-  // move to next question
-
-  // check if we've run out of questions
-    // if so, end the quiz
-    // else, get the next question
-}
-
-/// FUNCTION TO END THE QUIZ ///
-function quizEnd() {
-  // stop timer
-
-  // show end screen
-
-  // show final score
-
-  // hide questions section
-}
-
-/// FUNCTION FOR UPDATING THE TIME ///
-function clockTick() {
-  // update time
-
-  // check if user ran out of time
-}
-
-function saveHighscore() {
-  // get value of input box - for initials
-
-  // make sure value wasn't empty
-    // get saved scores from localstorage, or if not any, set to empty array
-
-    // format new score object for current user
-
-    // save to localstorage
-
-    // redirect to next page
-}
-
-/// CLICK EVENTS ///
-  // user clicks button to submit initials
-
-  // user clicks button to start quiz
-
-  // user clicks on element containing choices
+  
+  /// CLICK EVENT TO RUN THE CLEAR SCORES FUNCTION ///
+  
+  // run function when page loads
+  printHighscores();
